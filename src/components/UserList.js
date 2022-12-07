6e834e42249b51deeda072dbe7e1538da996bb83
bigmosi <kinyeramo@gmail.com>
2022-12-07 23:36:33 +0300

    added pagination

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,7 +1,9 @@
 // @flow
 import * as React from 'react'
 import styled from 'styled-components'
-import { useState } from 'react'
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import './UserList.css';
 
 import type { Kinja$User } from '../api/profile'
 
@@ -13,18 +15,38 @@ type Props = {
 
 export function UserList({ users }: Props): Array<React$Element<typeof UserItem>> {
     const [query, setQuery] = React.useState("");
+    const [currentPage, setCurrentPage] = useState(1);
+    const [usersPerPage] = useState(5);
+
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUser =  users.slice(indexOfFirstUser, indexOfLastUser);
+
+    const paginate = ({selected}) => {
+        setCurrentPage(selected + 1);
+    }
 
     return (
         <div>
             {users.length}
             <input type="search" value={query} placeholder="Search by role..." onChange={(e) => {setQuery(e.target.value)}} />
-            {users.filter((user) => {
+            {currentUser.filter((user) => {
                 if (query === "") {
                     return user;
                 } else if (user.role.toLowerCase().includes(query.toLowerCase())) {
                     return user;
                 }
             }).map(user => <UserItem {...user} />)}
-        </div>
+<ReactPaginate
+                  onPageChange={paginate}
+                  pageCount={Math.ceil(users.length / usersPerPage)}
+                  previousLabel={'Prev'}
+                  nextLabel={'Next'}
+                  containerClassName={'pagination'}
+                  pageLinkClassName={'page-number'}
+                  previousLinkClassName={'page-number'}
+                  nextLinkClassName={'page-number'}
+                  activeLinkClassName={'active'}
+               />        </div>
     )
 }
